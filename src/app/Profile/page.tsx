7d0cda5ff6db3ea80c96f698bbd8743fbc3041a7
manlikexavy <chibokaxavier@gmail.com>
2024-07-20 18:24:00 +0100

    file uploading and retrieving of url succesful

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -3,6 +3,13 @@ import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import { useRouter } from "next/navigation";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import app from "../../../firebase";
 
 const page = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
@@ -11,6 +18,11 @@ const page = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const fileRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formdata, setFormdata] = useState({});
+  console.log(formdata);
 
   if (!currentUser) {
     router.push("/Signin");
@@ -25,10 +37,52 @@ const page = () => {
     }
   }, [currentUser, router]);
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file: File = e.target.files[0];
+      setFile(file);
+    } else {
+      setFile(null); // or handle the case when no file is selected
+    }
+  };
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = (file: File) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          setFormdata({ ...formdata, avatar: downloadUrl });
+        });
+      }
+    );
+  };
+
   return (
     <div className="">
       <form className="flex lg:flex-row flex-col mt-10 md:mt-20 gap-10 md:justify-around  items-center">
-        <input type="file" ref={fileRef} hidden accept="image/*" />
+        <input
+          onChange={handleFileChange}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
           onClick={() => fileRef.current?.click()}
           src={currentUser.avatar}
