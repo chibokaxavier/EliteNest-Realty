9a46bc7ddb527c231d3bf2dddb103dcf94059220
manlikexavy <chibokaxavier@gmail.com>
2024-08-11 20:15:09 +0100

    installed input from shadcn

--- components/CreateUser.tsx
+++ components/CreateUser.tsx
@@ -4,6 +4,7 @@ import { z } from "zod";
 import React from "react";
 import { useForm } from "react-hook-form";
 import {
+    Form,
   FormControl,
   FormField,
   FormItem,
@@ -24,8 +25,32 @@ const CreateUser = () => {
     name,
     email,
     password,
-  }: z.infer<typeof UserFormValidation>) => {};
-  return <div></div>;
+  }: z.infer<typeof UserFormValidation>) => {
+    console.log({ name, email, password });
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default CreateUser;
